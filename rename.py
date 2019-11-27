00181bffcaaeafbda49cfb9dc2c0993a21c2a3f5
Timing <wangxiangwei.cv@gmail.com>
2019-11-26 21:29:00 -0500

    checkout update

--- rename.py
+++ rename.py
@@ -6,6 +6,7 @@ import os
 # python rename.py image_path target_path
 file_path = sys.argv[1]
 target_path = sys.argv[2]
+tag =sys.argv[3]
 if not os.path.exists(target_path):
         os.makedirs(target_path)
 file_names = sorted(glob.glob(file_path+'/*.png'))
@@ -14,7 +15,7 @@ i = 0
 for file_name in file_names:
     i_1 = i/1000
     i_2 = i%1000
-    newfile_name = target_path+'/a_'+str(i_1).zfill(3)+'_'+str(i_2).zfill(3)+'.png'
+    newfile_name = target_path+'/'+tag+'_'+str(i_1).zfill(3)+'_'+str(i_2).zfill(3)+'.png'
     print newfile_name
     shutil.copy(file_name,newfile_name)
     i = i+1
